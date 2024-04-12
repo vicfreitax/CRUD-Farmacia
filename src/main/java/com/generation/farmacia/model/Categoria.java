34ba80095d48cb777a1ebc23feefda579e2a4a18
vicfreitax <victoriafreitax@gmail.com>
2024-04-12 16:16:08 -0300

    CRUD PRODUTO

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,14 +1,19 @@
 package com.generation.farmacia.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,7 +27,7 @@ public class Categoria {
 	private Long id;
 
 	@NotBlank(message = "O atributo Nome é obrigatório!")
-	@Size(min=5, max=100, message ="O atributo título deve conter no mínimo 05 e no máximo 100 caracteres.")
+	@Size(min=5, max=100, message ="O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres.")
 	private String nome;
 	
 	@NotBlank(message = "O atributo descrição é obrigatório!")
@@ -31,6 +36,10 @@ public class Categoria {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("tema")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -62,6 +71,14 @@ public class Categoria {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 }
